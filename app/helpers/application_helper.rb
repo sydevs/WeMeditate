8f885b6e128c555171b8c74dd5b4792648573b84
Devindra Payment <devindra@shaw.ca>
2018-01-30 10:21:32 +0000

    Minor updates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,9 @@ module ApplicationHelper
     time_ago_in_words(duration_filter.minutes.minutes.from_now).titleize
   end
 
+  def markdown content
+    rc = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    rc.render(content).html_safe
+  end
+
 end
--- app/uploaders/track_uploader.rb
+++ app/uploaders/track_uploader.rb
@@ -41,7 +41,7 @@ class TrackUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  def filename
-    "track.mp3" if original_filename
-  end
+  #def filename
+  #  "track.mp3" if original_filename
+  #end
 end
