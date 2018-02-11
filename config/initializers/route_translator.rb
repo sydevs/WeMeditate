103b647b4ffe6ed2bf6b5c04a8c578b096d939a8
Devindra Payment <devindra@shaw.ca>
2018-02-11 21:34:52 +0100

    Fixed route translations

--- config/initializers/route_translator.rb
+++ config/initializers/route_translator.rb
@@ -1,9 +1,14 @@
 
 RouteTranslator.config do |config|
   config.locale_param_key = :locale
+  config.generate_unnamed_unlocalized_routes = true
+  
+  #config.verify_host_path_consistency = true
 
-  RouteTranslator.config.host_locales = {
-      '*.ru' => :ru,
-      '*'    => :en,
-  }
+  #RouteTranslator.config.host_locales = {
+  #  '*.ru' => :ru,
+  #  '*'    => :en,
+  #}
+
+  # TODO: See this comment for a workaround, https://github.com/enriclluelles/route_translator/issues/171#issuecomment-324083093
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,37 +4,37 @@ Rails.application.routes.draw do
 
   get 'switch_user' => 'switch_user#set_current_user'
   
-  scope "(:locale)" do
-  #localized do
-    root to: "application#front"
-    
-    namespace :admin do
-      root to: "application#dashboard"
+  scope ':locale' do
+    localized do
+      root to: 'application#front'
       
-      resources :users, :treatments, :meditations, :tracks,
-                only: [:index, :create, :update, :destroy]
+      namespace :admin do
+        root to: 'application#dashboard'
+        
+        resources :users, :treatments, :meditations, :tracks,
+                  only: [:index, :create, :update, :destroy]
 
-      resources :articles, :static_pages do
-        patch :review, on: :member
+        resources :articles, :static_pages do
+          patch :review, on: :member
+        end
+
+        resources :cities do
+          get :lookup, on: :collection, constraints: { format: 'json' }
+          patch :review, on: :member
+        end
+
+        resources :categories, :mood_filters, :instrument_filters, :goal_filters, :duration_filters, 
+                  only: [:index, :create, :update, :destroy] do
+          put :sort, on: :collection
+        end
       end
 
-      resources :cities do
-        get :lookup, on: :collection, constraints: { format: 'json' }
-        patch :review, on: :member
-      end
-
-      resources :categories, :mood_filters, :instrument_filters, :goal_filters, :duration_filters, 
-                only: [:index, :create, :update, :destroy] do
-        put :sort, on: :collection
-      end
+      resources :articles, :cities, only: [:show]
+      resources :categories, only: [:index, :show]
+      resources :meditations, only: [:show]
+      resources :treatments, only: [:index, :show]
+      #resources :static_pages, only: [:show], path: '/'
     end
-
-    resources :articles, :cities, only: [:show]
-    resources :static_pages, only: [:show], path: '/'
-    resources :categories, only: [:index, :show]
-    resources :meditations, only: [:show]
-    resources :treatments, only: [:index, :show]
   end
-  #end
 
 end
