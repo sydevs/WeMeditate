c91f1e48f77de06e19a032d51314dc15c2baecc9
Devindra Payment <devindra@shaw.ca>
2018-03-19 12:43:44 +0100

    Removed fog from config

--- app/uploaders/generic_uploader.rb
+++ app/uploaders/generic_uploader.rb
@@ -1,8 +1,4 @@
 class GenericImageUploader < CarrierWave::Uploader::Base
-  # Choose what kind of storage to use for this uploader:
-  # storage :file
-  storage :fog
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
