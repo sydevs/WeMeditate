2729fa0f2008a412eaa62cebf7c48ee0fa259cd0
Devindra Payment <devindra@shaw.ca>
2018-05-04 10:03:22 +0200

    Proper implementation of the subtle system chart

--- app/assets/javascripts/front/subtle_system.js
+++ app/assets/javascripts/front/subtle_system.js
@@ -4,12 +4,14 @@ const SubtleSystem = {
   sections: null,
   chakras_chart: null,
   channels_chart: null,
+  active_node: '',
 
   load: function() {
     console.log('loading SubtleSystem.js')
+    SubtleSystem.active_node = ''
     SubtleSystem.sections = $('section.type-special.format-subtle-system')
     SubtleSystem.sections.find('.filter').click(SubtleSystem._on_filter_click)
-    console.log(SubtleSystem.sections.find('.filter'))
+    SubtleSystem.sections.find('.trigger').mouseover(SubtleSystem._on_trigger_mouseover)
   },
 
   _on_filter_click: function() {
@@ -19,6 +21,19 @@ const SubtleSystem = {
     SubtleSystem.sections.find(filter).addClass('active')
     $(this).addClass('active')
   },
+
+  _on_trigger_mouseover: function() {
+    console.log('mouseover', this)
+    //$(SubtleSystem.active_node).stop().fadeOut('slow') //.removeClass('active')
+
+    $(SubtleSystem.active_node).removeClass('active')
+    SubtleSystem.active_node = '.' + $(this).data('class')
+
+    setTimeout(function() {
+      $(SubtleSystem.active_node).addClass('active')
+      //$(SubtleSystem.active_node).stop().fadeIn('slow') //.addClass('active')
+    }, 300)
+  }
 
 }
 
