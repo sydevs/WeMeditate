45708e77d97f00b3020de78969b8b95b7ba4e9bd
Devindra Payment <devindra@shaw.ca>
2018-05-23 18:55:55 +1000

    Updated seeding order

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -2,7 +2,7 @@
 include Sprig::Helpers
 
 sprig_shared [
-  StaticPage, Category, Article, City, Section, Treatment, SubtleSystemNode,
+  Category, Article, StaticPage, City, SubtleSystemNode, Treatment, Section,
   Artist, MoodFilter, InstrumentFilter, Track,
   DurationFilter, GoalFilter, Meditation
 ]
