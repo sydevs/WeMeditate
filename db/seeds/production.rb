8975bf267e8f42f83b0246955c6c841f6bb39e8e
Devindra Payment <devindra@shaw.ca>
2018-06-27 14:29:01 +1000

    Added attachment seed command

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -2,7 +2,7 @@
 include Sprig::Helpers
 
 sprig_shared [
-  Category, Article, StaticPage, City, SubtleSystemNode, Treatment, Section,
+  Category, Article, StaticPage, City, SubtleSystemNode, Treatment, Section, Attachment,
   Artist, MoodFilter, InstrumentFilter, Track,
   DurationFilter, GoalFilter, Meditation
 ]
