05e2400837e0c609f3a805e2739b2a352b21eeab
Devindra Payment <devindra@shaw.ca>
2018-12-30 18:00:58 +0000

    Updated footer markup

--- app/assets/javascripts/front/dropdown.js
+++ app/assets/javascripts/front/dropdown.js
@@ -6,7 +6,6 @@
 const Dropdown = {
   // Called when turbolinks loads the page
   load() {
-    console.log('loading Dropdown.js')
     $root = $(document)
     $root.on('click', '.dropdown-selection', Dropdown._on_click_dropdown)
     $root.on('click', '.dropdown-popup-close', Dropdown._on_click_dropdown)
@@ -15,13 +14,11 @@ const Dropdown = {
 
   // Triggered when the dropdown selection is clicked on.
   _on_click_dropdown(event) {
-    console.log('click dropdown')
     $dropdown = $(event.currentTarget).closest('.dropdown')
     Dropdown.toggle_popup($dropdown)
   },
 
   _on_click_anywhere(event) {
-    console.log('click anywhere', event)
     if ($(event.target).closest('.dropdown').length == 0) {
       $('.dropdown.expand').each(function() {
         Dropdown.toggle_popup($(this), false)
@@ -31,7 +28,6 @@ const Dropdown = {
 
   // Triggered when a dropdown item is selected
   _on_select_item(event) {
-    console.log('select item')
     let $target = $(event.currentTarget)
     let $dropdown = $target.closest('.dropdown')
     let $selection = $dropdown.children('.dropdown-selection')
@@ -52,7 +48,6 @@ const Dropdown = {
 
   // Open the dropdown popup if it is closed, and vice versa.
   toggle_popup($dropdown, state) {
-    console.log('toggle popup')
     if (typeof state === 'undefined') {
       state = !$dropdown.hasClass('expand')
     }
