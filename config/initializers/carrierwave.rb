0c09f111b21ffe13a07e3a9eb8f76cd27921634d
Devindra Payment <devindra@shaw.ca>
2019-01-27 20:53:00 +0000

    Debugging for carrierwave init

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,15 @@
 CarrierWave.configure do |config|
   config.storage = Rails.env.production? ? :gcloud : :file
+  puts "CARRIERWAVE STORAGE #{Rails.env.production? ? :gcloud : :file}"
   config.asset_host = ActionController::Base.asset_host
   config.gcloud_bucket = 'wemeditate.co'
   config.gcloud_bucket_is_public = true
   config.gcloud_authenticated_url_expiration = 600
 
-  config.gcloud_attributes = {
-    expires: 600
-  }
+  config.gcloud_attributes = { expires: 600 }
 
   config.gcloud_credentials = {
     gcloud_project: 'we-meditate',
     gcloud_keyfile: "#{Rails.root}/config/initializers/carrierwave.json"
-  }
+  } # TODO: This file has been leaked, reset the keyfile
 end
