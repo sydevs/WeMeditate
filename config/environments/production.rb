38da641ed4f474e0101e72eb32104316fe355378
Devindra Payment <devindra@shaw.ca>
2019-01-28 13:59:43 +0000

    Remove asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = 'http://wemeditate.co'
+  #config.action_controller.asset_host = 'http://wemeditate.co'
 
   # More secure csrf tokens
   config.action_controller.per_form_csrf_tokens = true
