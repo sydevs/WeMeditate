fbad0153cefacba57b15e32c2c4020bc8f66d49d
Devindra Payment <devindra@shaw.ca>
2019-01-28 12:51:45 +0000

    Updated gems and changed file config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_assets = true
+  config.public_file_server.enabled = true
   #config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
@@ -29,7 +29,7 @@ Rails.application.configure do
   config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true # TODO: This should be set back to false, and a suitable fix or alternative for inline_svg should be found.
+  config.assets.compile = false # TODO: This should be set back to false, and a suitable fix or alternative for inline_svg should be found.
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
