9092ff741c0391df1f7e13c824e9961048fef451
Devindra <arden.ived@gmail.com>
2019-07-15 19:15:56 +0100

    Fixed bug with maintenance page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -73,7 +73,7 @@ class ApplicationController < ActionController::Base
 
   # The page that users are redirected to if the site is in maintenance mode and they aren't logged in.
   def maintenance
-    render layout: :basic
+    render layout: 'basic'
   end
 
   def robots
