3babb25dc574fcfaa43624e7b4d4fd55c58b641c
Devindra <arden.ived@gmail.com>
2019-08-17 16:21:34 +0100

    Another fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -35,18 +35,8 @@ module ApplicationHelper
     return content_for(:content) if content_for?(:content)
     return unless record.parsed_content.present?
 
-    if action_name == 'preview' || !record.content_cache_key.present?
-      record.content_blocks.each do |block|
-        concat render "content_blocks/#{block['type']}_block", block: block['data'].deep_symbolize_keys
-      end
-    else
-      cache.fetch(record.content_cache_key) do
-        capture do
-          record.content_blocks.each do |block|
-            concat render "content_blocks/#{block['type']}_block", block: block['data'].deep_symbolize_keys
-          end
-        end
-      end
+    record.content_blocks.each do |block|
+      concat render "content_blocks/#{block['type']}_block", block: block['data'].deep_symbolize_keys
     end
   end
 
