9a417a039fb396d714dc1c7a1ba9940992340c9c
Devindra <arden.ived@gmail.com>
2019-09-08 17:28:14 +0100

    Media file test

--- app/uploaders/media_file_uploader.rb
+++ app/uploaders/media_file_uploader.rb
@@ -28,7 +28,7 @@ class MediaFileUploader < ApplicationUploader
 
   def get_metadata
     return nil unless file
-    width, height = ::MiniMagick::Image.open(file.file.path)[:dimensions]
+    width, height = ::MiniMagick::Image.open(file.file.url)[:dimensions]
     { width: width, height: height }
   end
 
