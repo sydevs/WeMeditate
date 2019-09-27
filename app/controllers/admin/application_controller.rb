092f563bf70661ffd2ade5d0754bf6abf9e6667a
Devindra <arden.ived@gmail.com>
2019-09-27 11:15:29 +0100

    Change vimeo logging

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -39,7 +39,7 @@ module Admin
         end
       
         response = JSON.parse(response.body)
-        puts "Retrieved Vimeo Data for #{vimeo_id}\r\n#{response.body.inspect}"
+        puts "Retrieved Vimeo Data for #{vimeo_id}\r\n#{response.inspect}"
       
         return {
           vimeo_id: vimeo_id,
