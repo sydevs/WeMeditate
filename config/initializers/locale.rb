1ee24c047e51b1dd982abaa47068a27dbd2c03a1
Devindra <arden.ived@gmail.com>
2019-10-18 10:31:43 +0100

    Disable ukrainian

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,6 +1,6 @@
 
 # When adding new locales, they must be appended to the end of the list or else the section :languages enum will be messed up.
-I18n.available_locales = %i[en ru it de fr es pt nl hy uk]
+I18n.available_locales = %i[en ru it de fr es pt nl hy] # uk]
 I18n.enforce_available_locales = true
 I18n.default_locale = :en
 
