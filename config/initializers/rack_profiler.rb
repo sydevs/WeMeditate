336d564b205d0f568bb2113cb0af1e7736aa6dc1
Devindra <arden.ived@gmail.com>
2019-10-30 08:06:59 +0000

    Set rack profiler position

--- config/initializers/rack_profiler.rb
+++ config/initializers/rack_profiler.rb
@@ -2,5 +2,6 @@ if Rails.env.development?
   require 'rack-mini-profiler'
 
   # initialization is skipped so trigger it
+  Rack::MiniProfiler.config.position = 'bottom-left'
   Rack::MiniProfilerRails.initialize!(Rails.application)
 end
