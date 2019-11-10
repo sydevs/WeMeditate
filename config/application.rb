f88670325d7a834ead10fe1f14eadb76e9db2093
David Backeus <duztdruid@gmail.com>
2019-11-10 17:57:09 +0100

    Bump defaults to 6.0 and regenerate rails config files
    After activating Zeitwerk we get some warnings from `initializers/audited.rb` (related to https://github.com/collectiveidea/audited/issues/501) and `initializers/carrierwave.rb` (which will be going away as we migrate to activestorage).

--- config/application.rb
+++ config/application.rb
@@ -11,11 +11,12 @@ module Wemeditate
   class Application < Rails::Application
 
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
     config.twitter_handle = '@wemeditate'
     config.exceptions_app = self.routes
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,7 @@ Rails.application.configure do
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
+    config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
@@ -52,7 +53,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  # Raises error for missing translations
+  # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
   config.i18n.fallbacks = false
 
