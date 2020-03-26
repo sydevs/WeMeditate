cf41d667846fdccbfb3036bc4923d380c600bed4
Devindra <arden.ived@gmail.com>
2020-03-26 10:31:06 +0000

    Link to online sessions

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -74,7 +74,7 @@ module NavigationHelper
     # Add classes near me
     mobile_navigation.push({
       title: I18n.translate('header.classes_near_me').gsub('<br>', ' '),
-      url: static_page_path_for(:classes),
+      url: static_page_path_for(:streams),
       data: gtm_label('header.classes_near_me'),
       active: controller_name == 'classes',
     })
