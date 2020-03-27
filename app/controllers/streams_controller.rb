c94b8852ace29ea16417b1a34788cdfee5463ca1
Devindra <arden.ived@gmail.com>
2020-03-27 14:48:37 +0000

    Update stream timing

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -3,23 +3,10 @@ class StreamsController < ApplicationController
   def index
     @static_page = StaticPage.preload_for(:content).find_by(role: :streams)
 
-    @countdown_time = Time.now.change(hour: 19)
-
-    if @countdown_time < Time.now
-      @countdown_time = Time.now.tomorrow.change(hour: 19)
-    end
-
-    if @countdown_time.saturday? || @countdown_time.sunday?
-      @countdown_time = @countdown_time.monday.next_week.change(hour: 19)
-    end
-
-    # First date is Friday, March 27
-    if @countdown_time < DateTime.parse('2020-03-27')
-      @countdown_time = DateTime.parse('2020-03-27').to_time.change(hour: 20)
-    end
+    @countdown_time = countdown_target_time
 
     seconds_diff = (@countdown_time - Time.now).to_i
-    @live = params[:live] || seconds_diff.negative?
+    @live = params[:live] || seconds_diff > 5.minutes
 
     if @live
       @stream_url = "https://player.twitch.tv/?channel=wemeditate&parent=#{request.host}"
@@ -38,5 +25,23 @@ class StreamsController < ApplicationController
 
     set_metadata(@static_page)
   end
+
+  private
+
+    def countdown_target_time
+      countdown_time = next_stream_time(Date.today)
+      countdown_time = next_stream_time(Date.tomorrow) if Time.now > countdown_time + 30.minutes
+      countdown_time
+    end
+
+    def next_stream_time date
+      if date < DateTime.parse('2020-03-27')
+        date = DateTime.parse('2020-03-27')
+      elsif date.saturday? || date.sunday?
+        date = date.monday
+      end
+
+      date.to_time.change(hour: 15)
+    end
 
 end
