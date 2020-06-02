e74bc0f4bddce83a6170aefcaa84d554965076b3
Devindra <arden.ived@gmail.com>
2020-06-02 08:49:38 +0100

    Remove caching on splash block

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -33,7 +33,7 @@ class StreamsController < ApplicationController
   private
 
     def set_cache_headers
-      response.headers["Cache-Control"] = "no-cache, no-store"
+      response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
       response.headers["Pragma"] = "no-cache"
       response.headers["Expires"] = "Mon, 01 Jan 1990 00:00:00 GMT"
     end
