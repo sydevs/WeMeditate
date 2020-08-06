37c062ac9a8a7cbbead6e4aec07b575988e30de7
Devindra <arden.ived@gmail.com>
2020-08-06 09:32:05 +0100

    Add migration to preserve current URLs

---
+++ db/migrate/20200806080445_save_historic_slugs.rb
@@ -0,0 +1,7 @@
+class SaveHistoricSlugs < ActiveRecord::Migration[6.0]
+  def change
+    [StaticPage, Article, SubtleSystemNode, Meditation, Stream, Treatment].each do |model|
+      model.find_each.select(&:save)
+    end
+  end
+end
