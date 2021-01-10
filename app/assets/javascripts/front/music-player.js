65905165ce440e01f32e6f2da81d7a68d55396b4
Mrinal Singhal <mrinalsinghal97@gmail.com>
2021-01-10 20:24:37 +0530

    Artwork fix

--- app/assets/javascripts/front/music-player.js
+++ app/assets/javascripts/front/music-player.js
@@ -146,12 +146,12 @@ class MusicPlayer {
         title: data.name,
         artist: this.artistsList.innerText,
         artwork: [
-          { src: 'https://dummyimage.com/96x96', sizes: '96x96', type: 'image/png' },
-          { src: 'https://dummyimage.com/128x128', sizes: '128x128', type: 'image/png' },
-          { src: 'https://dummyimage.com/192x192', sizes: '192x192', type: 'image/png' },
-          { src: 'https://dummyimage.com/256x256', sizes: '256x256', type: 'image/png' },
-          { src: 'https://dummyimage.com/384x384', sizes: '384x384', type: 'image/png' },
-          { src: 'https://dummyimage.com/512x512', sizes: '512x512', type: 'image/png' },
+          { src: data.cover_art_url, sizes: '96x96', type: 'image/jpg' },
+          { src: data.cover_art_url, sizes: '128x128', type: 'image/jpg' },
+          { src: data.cover_art_url, sizes: '192x192', type: 'image/jpg' },
+          { src: data.cover_art_url, sizes: '256x256', type: 'image/jpg' },
+          { src: data.cover_art_url, sizes: '384x384', type: 'image/jpg' },
+          { src: data.cover_art_url, sizes: '512x512', type: 'image/jpg' },
         ]
       })
 
