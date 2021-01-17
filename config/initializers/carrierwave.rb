f3042af0f9929d72cfcc2ebec271137fe34e00c5
Devindra <arden.ived@gmail.com>
2021-01-17 10:22:21 -0800

    Fix fog config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,12 +3,14 @@ CarrierWave.configure do |config|
 
   if ENV['GCLOUD_BUCKET'].present?
     config.storage = :fog
+
     config.fog_provider = 'fog/google'
     config.fog_directory = ENV['GCLOUD_BUCKET']
+    config.fog_attributes = { expires: 600 }
     config.fog_credentials = {
-        provider:               'Google',
-        google_project:         'we-meditate',
-        google_json_key_string: ENV['GOOGLE_CLOUD_KEYFILE'].present? ? JSON.parse(ENV['GOOGLE_CLOUD_KEYFILE']) : nil,
+      provider:               'Google',
+      google_project:         'we-meditate',
+      google_json_key_string: ENV['GOOGLE_CLOUD_KEYFILE'].present? ? ENV['GOOGLE_CLOUD_KEYFILE'] : nil,
     }
 =begin
     config.storage = :gcloud
