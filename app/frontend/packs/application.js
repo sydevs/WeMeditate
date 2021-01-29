ec79593a122789696e21243fba9c713e5e4daa63
Devindra <arden.ived@gmail.com>
2021-01-28 16:02:49 -0800

    Update stylesheets location

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -8,4 +8,9 @@ import "regenerator-runtime/runtime"
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker')
+/**
+ * Refers to app/javascript/stylesheets/application.scss (or application.css)
+ * Note that we don't need to preface this path with "app/javascript" due to the `source_path` config set in config/webpacker.yml. Magical!
+ * The file extension can be left off due to the `extensions` config in config/webpacker.yml.
+ */
+import "stylesheets/application"
