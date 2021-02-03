8011302446658c51f26b7d3990405ed5620599cb
Devindra <arden.ived@gmail.com>
2021-02-03 08:37:22 -0800

    Add images to webpacker pipeline

--- app/frontend/packs/admin.js
+++ app/frontend/packs/admin.js
@@ -5,12 +5,12 @@ import 'regenerator-runtime/runtime'
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-const images = require.context('../images', true)
-const imagePath = (name) => images(name, true)
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
 
 /**
  * Refers to app/javascript/stylesheets/application.scss (or application.css)
  * Note that we don't need to preface this path with "app/javascript" due to the `source_path` config set in config/webpacker.yml. Magical!
  * The file extension can be left off due to the `extensions` config in config/webpacker.yml.
  */
-import '../stylesheets/public.scss'
+import '../stylesheets/admin.scss'
