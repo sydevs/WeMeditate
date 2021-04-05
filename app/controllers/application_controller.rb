6c654abb26246728ef15e059bc0ead848c4c8bb0
David Backeus <duztdruid@gmail.com>
2021-04-05 17:19:59 +0200

    Fix sitemap generation (#90)
    ### Why
    
    Running the daily `rake sitemap:refresh` to generate new sitemap versions fails: https://rollbar.com/sydevelopers/WeMeditate/items/283/
    
    Also the `ApplicationController#sitemap` action is referencing a legacy sitemap bucket which hasn't been updated since Jan 2020.
    
    ### What
    
    - Use sitemap_generator’s built in GoogleStorageAdapter (added in 6.1)
    - Use the same bucket in ApplicationController#sitemap as in config/sitemap.rb
    - Upgrade sitemap_generator to 6.1.2. This fixes a Ruby 2.7.2 related deprecation warning:
    https://github.com/kjvarga/sitemap_generator/blob/master/CHANGES.md#611
    - Upgrade google-cloud-storage and related gems to latest versions
    - Rely on conventional ENV variables for google-cloud-storage configuration, note: I added GOOGLE_CLOUD_PROJECT=wemeditate to Heroku apps (reference: https://googleapis.dev/ruby/google-cloud-storage/latest/file.AUTHENTICATION.html#environment-variables)
    
    Also update puma since the old version failed to compile native extensions in my MacOS environment.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -98,12 +98,8 @@ class ApplicationController < ActionController::Base
     expires_in 1.day, public: true
 
     # The sitemap itself is hosted on Google Cloud storage, we read it from them and send it back to the accessor of this endpoint.
-    storage = Google::Cloud::Storage.new({
-      project_id: 'we-meditate',
-      credentials: ENV['GOOGLE_CLOUD_KEYFILE'].present? ? JSON.parse(ENV['GOOGLE_CLOUD_KEYFILE']) : nil,
-    })
-
-    bucket = storage.bucket 'wemeditate'
+    storage = Google::Cloud::Storage.new
+    bucket = storage.bucket ENV.fetch('GCLOUD_BUCKET')
     file = bucket.file "sitemaps/sitemap.#{I18n.locale}.xml.gz"
 
     send_data file.download.read, type: 'text/xml'
