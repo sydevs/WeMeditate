c3b506639144225152e5c4cdc4a9a21cfa9773f3
Devindra <arden.ived@gmail.com>
2021-04-20 12:40:38 -0700

    Restore old carrierwave config

--- Gemfile
+++ Gemfile
@@ -40,7 +40,7 @@ gem 'array_enum' # Allows the use of enum arrays
 
 # Uploads
 gem 'carrierwave' # Core support for file uploads
-gem "fog-google" # Let's us store the files in Google Storage
+gem 'carrierwave-google-storage' # Let's us store the files in Google Storage
 gem 'carrierwave-meta' # To get image meta data
 gem 'google-cloud-storage' # Needed to access sitemaps
 gem 'mini_magick' # Image processing during upload
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,15 +2,22 @@ CarrierWave.configure do |config|
   config.asset_host = "https://#{ENV['GCLOUD_BUCKET']}" if ENV['GCLOUD_BUCKET']
 
   if ENV['GCLOUD_BUCKET'].present?
-    config.fog_provider = 'fog/google'
-    config.fog_directory = ENV['GCLOUD_BUCKET']
-    config.fog_attributes = { expires: 600 }
-    config.fog_credentials = {
-      provider:               'Google',
-      google_project:         'we-meditate',
-      google_json_key_string: ENV['GOOGLE_CLOUD_KEYFILE'].present? ? ENV['GOOGLE_CLOUD_KEYFILE'] : nil,
+    # If the bucket name looks like a host name use it as asset_host.
+    # For example "wemeditate" is treated as bucket only with URL's like:
+    # https://storage.googleapis.com/wemeditate/uploads/<carrier-wave-path>
+    # while "assets.wemeditate.co" is treated as a host with URL's like:
+    # https://assets.wemeditate.co/uploads/<carrier-wave-path>
+    config.gcloud_bucket = ENV['GCLOUD_BUCKET']
+    config.asset_host = "https://#{ENV['GCLOUD_BUCKET']}" if ENV['GCLOUD_BUCKET'].include?('.')
+    config.gcloud_bucket_is_public = true
+    config.gcloud_authenticated_url_expiration = 600
+    config.gcloud_attributes = { expires: 600 }
+    config.gcloud_credentials = {
+      gcloud_project: 'we-meditate',
+      gcloud_keyfile: ENV['GOOGLE_CLOUD_KEYFILE'].present? ? JSON.parse(ENV['GOOGLE_CLOUD_KEYFILE']) : nil,
     }
-    config.storage = :fog
+
+    config.storage = :gcloud
   else
     config.storage = :file
   end
