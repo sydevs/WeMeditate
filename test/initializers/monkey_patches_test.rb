972a933b6f17d3de55f3158a4858a590ba183c65
David Backeus <duztdruid@gmail.com>
2021-07-02 18:18:56 +0200

    Add asset version query string for asset URL’s
    Allows us to bypass some kind of caching issue in Google Cloud Storage which prevented assets from responding with the correct CORS headers.

---
+++ test/initializers/monkey_patches_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class MonkeyPatchesTest < ActiveSupport::TestCase
+
+  test 'public_url of GcloudFile uploads includes asset version query string' do
+    CarrierWave.configure do |config|
+      config.storage = :gcloud
+    end
+
+    stub_request(:post, 'https://oauth2.googleapis.com/token')
+      .to_return(
+        status: 200,
+        body: { access_token: 'foo', refresh_token: 'bar' }.to_json,
+        headers: { "Content-Type": 'application/json' }
+      )
+    file_name = 'test.svg'
+    result = MediaFile.insert(
+      file: file_name,
+      page_type: 'StaticPage',
+      page_id: 1,
+      image_meta: { width: 640, height: 480 }.to_json,
+      created_at: Time.now,
+      updated_at: Time.now
+    )
+
+    media_file = MediaFile.find(result.first.fetch('id'))
+
+    host = "https://#{ENV.fetch('GCLOUD_BUCKET')}"
+    query_string = "version=#{Rails.application.config.assets.version}"
+    expected_url = "#{host}/uploads/media_file/file/#{media_file.id}/#{file_name}?#{query_string}"
+
+    assert_equal expected_url, media_file.file.url
+  end
+
+end
