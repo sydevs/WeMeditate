409cdc988218caca7d96ee95031ba907f1e447a3
Devindra <arden.ived@gmail.com>
2021-07-17 19:32:11 -0700

    Update webpacker and other packages

--- app/frontend/packs/public.js
+++ app/frontend/packs/public.js
@@ -4,9 +4,14 @@ import Turbolinks from 'turbolinks'
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 
+function importAll(r) {
+  r.keys().forEach(r)
+}
+
 // Copy all static fonts and images to the output folder
 const images = require.context('../images', true)
-const imagePath = (name) => images(name, true)
+//const imagePath = (name) => images(name, true)
+importAll(images)
 
 // Load CSS
 import '../stylesheets/public.scss'
