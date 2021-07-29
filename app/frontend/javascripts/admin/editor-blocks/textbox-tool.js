6cb93c1f0390d46a5b89e329ca5d6bfb84e5cb10
Devindra <arden.ived@gmail.com>
2021-07-28 18:56:57 -0700

    Fix textbox ornate background style

--- app/frontend/javascripts/admin/editor-blocks/textbox-tool.js
+++ app/frontend/javascripts/admin/editor-blocks/textbox-tool.js
@@ -26,7 +26,7 @@ export default class TextTool extends EditorTool {
       alignment: ['left', 'center', 'right'].includes(data.alignment) ? data.alignment : 'left',
       style: ['quote', 'poem'].includes(data.style) ? data.style : 'quote',
       spacing: ['overlap', 'separate'].includes(data.spacing) ? data.spacing : 'overlap',
-      background: ['white', 'image', 'brown'].includes(data.background) ? data.background : 'white',
+      background: ['white', 'image', 'ornate'].includes(data.background) ? data.background : 'white',
       color: ['dark', 'light'].includes(data.color) ? data.color : 'light',
       position: ['left', 'right'].includes(data.position) ? data.position : 'left',
       decorations: data.decorations || {},
@@ -75,7 +75,7 @@ export default class TextTool extends EditorTool {
           options: [
             { name: 'white', icon: 'square outline' },
             { name: 'image', icon: 'image outline' },
-            { name: 'brown', icon: 'university' },
+            { name: 'ornate', icon: 'university' },
           ]
         },
         spacing: {
