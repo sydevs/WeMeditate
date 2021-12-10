feb0390f2ae5ae973a7d15f254e2eceefd79826d
Devindra <arden.ived@gmail.com>
2021-12-10 10:42:35 +0100

    Fix static page urls

--- app/helpers/static_page_helper.rb
+++ app/helpers/static_page_helper.rb
@@ -3,6 +3,16 @@
 
 module StaticPageHelper
 
+  ROLE_TO_URL = {
+    home: :root,
+    subtle_system: :subtle_system_nodes,
+    articles: :categories,
+    treatments: :treatments,
+    tracks: :tracks,
+    meditations: :meditations,
+    streams: :streams,
+  }.freeze
+
   # Fetch a preview of the static page for a given role
   def self.preview_for role
     # Simply fetch all previews and store them, because we know that every page load will require almost every static page preview to generate the navigation
@@ -22,28 +32,17 @@ module StaticPageHelper
   # This helper fetches the path for one of those special pages, or the default URL for a given role or static page.
   def static_page_path_for page_or_role
     role = page_or_role.is_a?(StaticPage) ? page_or_role.role : page_or_role
-    case role&.to_sym
-    when :home
-      root_path
-    when :subtle_system
-      subtle_system_nodes_path
-    when :articles
-      categories_path
-    when :treatments
-      treatments_path
-    when :tracks
-      tracks_path
-    when :meditations
-      meditations_path
-    when :streams
-      live_path
-    when :custom
+    role = role&.to_sym
+
+    if ROLE_TO_URL.key?(role)
+      send(:"#{ROLE_TO_URL[role]}_#{Globalize.locale}_path")
+    elsif role == :custom
       page_or_role.is_a?(StaticPage) ? static_page_path(page_or_role) : nil
-    when nil
+    elsif role == nil
       ''
     else
       page = StaticPage.preload_for(:preview).find_by_role(role)
-      static_page_path(page)
+      polymorphic_path([page, Globalize.locale])
     end
   end
 
@@ -51,28 +50,17 @@ module StaticPageHelper
   # This helper fetches the URL for one of those special pages, or the default URL for a given role or static page.
   def static_page_url_for page_or_role
     role = page_or_role.is_a?(StaticPage) ? page_or_role.role : page_or_role
-    case role&.to_sym
-    when :home
-      root_url
-    when :subtle_system
-      subtle_system_nodes_url
-    when :articles
-      categories_url
-    when :treatments
-      treatments_url
-    when :tracks
-      tracks_url
-    when :meditations
-      meditations_url
-    when :streams
-      live_url
-    when :custom
+    role = role&.to_sym
+
+    if ROLE_TO_URL.key?(role)
+      send(:"#{ROLE_TO_URL[role]}_#{Globalize.locale}_url")
+    elsif role == :custom
       page_or_role.is_a?(StaticPage) ? static_page_url(page_or_role) : nil
-    when nil
+    elsif role == nil
       ''
     else
       page = StaticPage.preload_for(:preview).find_by_role(role)
-      static_page_url(page)
+      polymorphic_url([page, Globalize.locale])
     end
   end
 
