769817ae60aa92b3980e529df9495df24f618174
Devindra <arden.ived@gmail.com>
2022-04-28 11:27:40 +0100

    Restore the ability to retrieve vimeo metadata

--- app/frontend/javascripts/admin/application.js
+++ app/frontend/javascripts/admin/application.js
@@ -82,7 +82,61 @@ export function init(scope) {
   })
 
   // Add callbacks for a few input types
-  //scope.find('input[type=file]').on('change', event => this.onChangeFileInput(event.target))
-  //scope.find('.js-vimeo-field input').on('change', event => this.onRefreshVimeoInput(event.target.parentNode.parentNode))
-  //scope.find('.js-vimeo-field + .preview-item .reload').on('click', event => this.onRefreshVimeoInput(event.target.parentNode.previousSibling))
+  scope.find('input[type=file]').on('change', event => onChangeFileInput(event.target))
+  scope.find('.js-vimeo-field input').on('change', event => onRefreshVimeoInput(event.target.parentNode.parentNode))
+  scope.find('.js-vimeo-field + .preview-item .reload').on('click', event => onRefreshVimeoInput(event.target.parentNode.previousSibling))
+}
+
+// DEPRECATED VIMEO FUNCTIONS
+// TODO: Remove this code
+// When a file input has it's file changes, we need to update the preview.
+function onChangeFileInput(input) {
+  const $img = $(input).next('.image').children('img')
+
+  if ($img.length > 0 && input.files && input.files[0]) {
+    const reader = new FileReader()
+    reader.onload = function(event) {
+      $img.attr('src', event.target.result)
+    }
+
+    reader.readAsDataURL(input.files[0])
+  }
+}
+
+// When vimeo metadata is refreshed, we need to make the appropriate server requests
+function onRefreshVimeoInput(field) {
+  const vimeo_id = field.querySelector('input').value
+  const input = field.querySelector('.input')
+  const meta = field.nextSibling
+
+  if (!vimeo_id || isNaN(vimeo_id)) {
+    // If the vimeo id isn't valid, then hide this whole section
+    meta.querySelector('.content').style.display = 'none'
+  } else {
+    meta.querySelector('.content').style.display = null
+    input.classList.add('loading')
+
+    retrieveVimeoVideo(vimeo_id, response => {
+      // Render a preview of the returned meta data, and store it to an input to be saved.
+      meta.querySelector('.raw').innerText = JSON.stringify(response, null, 2)
+      meta.querySelector('img').src = response.thumbnail
+      meta.querySelector('.hint').innerText = response.title
+      meta.querySelector('a').href = `https://vimeo.com/${response.vimeo_id}`
+      meta.querySelector('input').value = JSON.stringify(response)
+
+      input.classList.remove('loading')
+    })
+  }
+}
+
+// Requests vimeo metadata from our server's vimeo data endpoint
+function retrieveVimeoVideo(vimeo_id, callback) {
+  $.ajax({
+    url: `/${window.locale}/vimeo_data?vimeo_id=${vimeo_id}`,
+    type: 'GET',
+    dataType: 'json',
+    success: function(result) {
+      callback(result)
+    },
+  })
 }
