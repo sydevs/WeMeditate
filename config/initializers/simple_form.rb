388086b56d52fe41112ccadb8c418d13a825c2ff
David Backeus <duztdruid@gmail.com>
2022-05-12 17:36:06 +0200

    Update and remove some unused gems
    Note on removals:
    - rails_serve_static_assets has been replaced by RAILS_SERVE_STATIC_FILES env var
    - redis isn't actually used for anything
    - jsonb_accessor no longer used for anything
    
    Locked audited since 5.x raised errors seemingly related to globalize
    
    To avoid action_cable failing to require Redis in production I switched to explicit require of individual rails libraries
    
    Full list of changes:
    actioncable (6.0.4.4 - 6.0.5)
    actionmailbox (6.0.4.4 - 6.0.5)
    actionmailer (6.0.4.4 - 6.0.5)
    actionpack (6.0.4.4 - 6.0.5)
    actiontext (6.0.4.4 - 6.0.5)
    actionview (6.0.4.4 - 6.0.5)
    activejob (6.0.4.4 - 6.0.5)
    activemodel (6.0.4.4 - 6.0.5)
    activerecord (6.0.4.4 - 6.0.5)
    activestorage (6.0.4.4 - 6.0.5)
    activesupport (6.0.4.4 - 6.0.5)
    array_enum (1.2.0 - 1.4.0)
    autoprefixer-rails (10.4.2.0 - 10.4.7.0)
    bcrypt (3.1.13 - 3.1.17)
    binding_of_caller (0.8.0 - 1.0.0)
    browser (4.0.0 - 5.3.1)
    byebug (11.1.1 - 11.1.3)
    capybara (3.31.0 - 3.37.1)
    childprocess (3.0.0 - 4.1.0)
    cloudflare-rails (0.6.0 - 2.4.0)
    concurrent-ruby (1.1.9 - 1.1.10)
    debug_inspector (0.0.3 - 1.1.0)
    dotenv (2.7.5 - 2.7.6)
    dotenv-rails (2.7.5 - 2.7.6)
    faraday (1.3.0 - 1.10.0)
    faraday-em_http (1.0.0)
    faraday-em_synchrony (1.0.0)
    faraday-excon (1.1.0)
    faraday-httpclient (1.0.1)
    faraday-multipart (1.0.3)
    faraday-net_http_persistent (1.2.0)
    faraday-patron (1.0.0)
    faraday-rack (1.0.0)
    faraday-retry (1.0.3)
    friendly_id (5.3.0 - 5.4.2)
    geocoder (1.6.1 - 1.7.5)
    globalize (5.3.0 - 6.1.0)
    httparty (0.18.0 - 0.20.0)
    inline_svg (1.7.0 - 1.8.0)
    jquery-rails (4.3.5 - 4.4.0)
    json (2.3.0 - 2.6.1)
    jwt (2.2.2 - 2.3.0)
    kaminari (1.2.1 - 1.2.2)
    kaminari-actionview (1.2.1 - 1.2.2)
    kaminari-activerecord (1.2.1 - 1.2.2)
    kaminari-core (1.2.1 - 1.2.2)
    letter_opener (1.7.0 - 1.8.1)
    listen (3.2.1 - 3.7.1)
    lograge (0.11.2 - 0.12.0)
    loofah (2.13.0 - 2.18.0)
    mail_form (1.8.0 - 1.9.0)
    matrix (0.4.2)
    memory_profiler (0.9.14 - 1.0.0)
    mime-types (3.3.1 - 3.4.1)
    mime-types-data (3.2021.0225 - 3.2022.0105)
    mini_magick (4.10.1 - 4.11.0)
    nokogiri (1.13.4 - 1.13.6)
    os (1.1.1 - 1.1.4)
    pg (1.2.2 - 1.3.5)
    public_suffix (4.0.6 - 4.0.7)
    rack-mini-profiler (2.0.2 - 3.0.0)
    rack-proxy (0.7.0 - 0.7.2)
    rails (6.0.4.4 - 6.0.5)
    rails_real_favicon (0.1.0 - 0.1.1)
    railties (6.0.4.4 - 6.0.5)
    rb-fsevent (0.10.3 - 0.11.1)
    recaptcha (5.5.0 - 5.10.0)
    regexp_parser (1.7.0 - 2.4.0)
    representable (3.0.4 - 3.1.1)
    request_store (1.5.0 - 1.5.1)
    responders (3.0.0 - 3.0.1)
    route_translator (10.0.0 - 12.1.0)
    rubyzip (2.2.0 - 2.3.2)
    selenium-webdriver (3.142.7 - 4.1.0)
    signet (0.15.0 - 0.16.1)
    simple_form (5.0.2 - 5.1.0)
    simple_po_parser (1.1.6)
    sitemap_generator (6.1.2 - 6.2.1)
    slim (4.0.1 - 4.1.0)
    slim-rails (3.2.0 - 3.5.1)
    sprig (0.3.0 - 0.3.1)
    sprockets (4.0.2 - 4.0.3)
    trailblazer-option (0.1.2)
    warden (1.2.8 - 1.2.9)
    web-console (4.0.1 - 4.2.0)
    webdrivers (4.2.0 - 5.0.0)
    webmock (3.13.0 - 3.14.0)
    webpacker (5.4.0 - 5.4.3)
    zeitwerk (2.5.3 - 2.5.4)

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -200,7 +200,7 @@ SimpleForm.setup do |config|
   # config.label_class = nil
 
   # You can define the class to use on all forms. Default is simple_form.
-  config.form_class = 'ui form'
+  config.default_form_class = 'ui form'
 
   # You can define which elements should obtain additional classes
   # config.generate_additional_classes_for = [:wrapper, :label, :input]
