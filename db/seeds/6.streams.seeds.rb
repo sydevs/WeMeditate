69e5c581a960db3116c7a63d0b5dc61f2a0acf8a
David Backeus <duztdruid@gmail.com>
2022-05-12 18:09:35 +0200

    Avoid exception raised while seeding streams
    Fixes:
    ActiveRecord::RecordNotSaved: You cannot call create unless the parent is saved

--- db/seeds/6.streams.seeds.rb
+++ db/seeds/6.streams.seeds.rb
@@ -10,24 +10,23 @@ streams = {}
   slug: 'main',
   excerpt: sentences(4),
   stream_url: 'www.google.com',
+  locale: :en,
   location: 'London',
   start_date: 1.week.ago.monday,
   start_time: '17:00',
   end_time: '18:00',
   recurrence: %i[monday],
+  state: :published,
   time_zone_identifier: 'Europe/London',
   time_zone_offset: 0,
   target_time_zones: [0],
-}].each_with_index do |atts, index|
-  stream = Stream.find_or_initialize_by(name: atts[:name])
-  atts.merge!({
-    state: :published,
-    locale: :en,
-    thumbnail_id: attachment('classes/streams.jpg', stream),
-  })
-  stream.update!(atts)
+}].each_with_index do |attributes, index|
+  stream = Stream.find_or_initialize_by(name: attributes[:name])
+  stream.assign_attributes(attributes)
+  stream.save! # NOTE: record must be saved to assign thumbnail attachment
+  stream.update!(thumbnail_id: attachment('classes/streams.jpg', stream))
   streams[index] = stream
-  puts "Created Stream - #{atts[:name]}"
+  puts "Created Stream - #{attributes[:name]}"
 end
 
 puts ' -- Finished Stream Seeds -- '
