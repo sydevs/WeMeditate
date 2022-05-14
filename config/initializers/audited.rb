d562da0d3af21fcc361ef450882855670db0185b
David Backeus <duztdruid@gmail.com>
2022-05-14 15:12:11 +0200

    Bumped to Rails 7 and some gems / fixes required to boot and run tests
    - Added bootsnap for faster app boot (has been default gem for many years now)
    - Updated config files with rails app:update
    - Locked audited gem to specific Github reference, after relaxing rails dependency requirements to support 7.x but before breaking boot time
    - Wrapped Audited initializer in to_prepare to allow autoloading Audit model class
    - Reference CarrierWave::Uploader::Base.asset_host instead of ApplicationUploader.asset_host to avoid autoload error on ApplicationUploader

--- config/initializers/audited.rb
+++ config/initializers/audited.rb
@@ -1,5 +1,7 @@
-
-Audited.config do |config|
-  config.audit_class = Audit
-  config.max_audits = 50
+# Needs to_prepare block since we're directly referencing the Audit model
+Rails.application.config.to_prepare do
+  Audited.config do |config|
+    config.audit_class = Audit
+    config.max_audits = 50
+  end
 end
