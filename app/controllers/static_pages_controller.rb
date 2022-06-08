cd5daddcae660c7d5e91719b767ca9367d6c4cbe
Devindra <arden.ived@gmail.com>
2022-06-08 19:41:35 +0200

    Unescape static page slug

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,8 @@
 class StaticPagesController < ApplicationController
 
   def show
-    slug = request.path.split('/').last
-    puts "STATIC PAGE SLUG #{slug}"
+    slug = CGI.unescape(request.path.split('/').last)
+    puts "STATIC PAGE SLUG #{slug} from #{request.path}"
     @static_page = StaticPage.publicly_visible.preload_for(:content).find_by_slug(slug)
     return if redirect_legacy_url(@static_page)
     return redirect_to helpers.static_page_path(@static_page) unless helpers.static_page_path(@static_page) == request.path
